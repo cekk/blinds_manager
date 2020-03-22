ff7815a8e73c539623e439c7c826420117420fe7
cekk <andrea.cecchi85@gmail.com>
2020-03-22 16:14:29 +0100

    new conf and styles

---
+++ static/build/precache-manifest.21728e9924f2b09d7a9478962ca083f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af729ca68ced99df83bd5ca43f92232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3ef0b1d55b4da03edf",
+    "url": "/static/js/2.fb5972d4.chunk.js"
+  },
+  {
+    "revision": "74777e1e2dda647a60b1",
+    "url": "/static/js/main.5914897b.chunk.js"
+  },
+  {
+    "revision": "268f0192eeb15de1a820",
+    "url": "/static/js/runtime-main.7db7bb0f.js"
+  }
+]);
